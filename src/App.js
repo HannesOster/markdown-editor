ecf00dcee704bae1e8ae49a5d674c74e49e10f46
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-09-24 15:29:25 +0200

    insert code from codepen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  const [markdown, setMarkdown] = useState(`# Heading 1
+    // ... (dein Markdown hier) ...`);
+
+  const handleInputChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  useEffect(() => {
+    document.getElementById("preview").innerHTML = marked(markdown);
+  }, [markdown]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-6">
+          <h5>Editor</h5>
+          <textarea
+            id="editor"
+            className="form-control"
+            rows="10"
+            value={markdown}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <div className="col-md-6">
+          <h5>Preview</h5>
+          <div id="preview"></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
